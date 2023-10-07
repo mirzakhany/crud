94a71c845c368e30402bdee5cff435a481dea03f
Mohsen Mirzakhani <mohsen.mkh88@gmail.com>
2023-10-07 23:28:46 +0200

    add more test models

--- migrations/00_init.up.sql
+++ migrations/00_init.up.sql
@@ -20,3 +20,28 @@ create table if not exists organizations (
     created_at timestamp not null default now(),
     updated_at timestamp not null default now()
 );
+
+create table if not exists api_keys (
+    id serial primary key,
+    name text not null,
+    key text not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now()
+);
+
+create table if not exists settings (
+    id serial primary key,
+    name text not null,
+    value text not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now()
+);
+
+create table if not exists tasks (
+    id serial primary key,
+    name text not null,
+    description text not null,
+    status text not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp not null default now()
+);
